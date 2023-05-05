88d001ce645d162c2cbc8e99d6d2ea82b728e89d
Leo Sun <leosun@Leos-MacBook-Pro.local>
2023-05-06 00:04:24 +0800

    refactor to use repository

---
+++ tacocloud-api/src/main/java/tacos/api/IngredientController.java
@@ -0,0 +1,46 @@
+package tacos.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import tacos.data.IngredientRepository;
+import tacos.domain.Ingredient;
+
+@RestController
+@RequestMapping(path = "/api/ingredients", produces = MediaType.APPLICATION_JSON_VALUE)
+@CrossOrigin
+public class IngredientController {
+	
+	private final IngredientRepository repo;
+
+	public IngredientController(IngredientRepository repo) {
+		this.repo = repo;
+	}
+	
+	@GetMapping
+	public Iterable<Ingredient> allIngredients() {
+		return repo.findAll();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Ingredient saveIngredient(@RequestBody Ingredient ingredient) {
+		return repo.save(ingredient);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteIngredient(@PathVariable("id") String ingredientId) {
+		repo.deleteById(ingredientId);
+	}
+	
+}
--- tacocloud-domain/src/main/java/tacos/domain/Ingredient.java
+++ tacocloud-domain/src/main/java/tacos/domain/Ingredient.java
@@ -1,11 +1,24 @@
 package tacos.domain;
 
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-public class Ingredient {
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+public class Ingredient implements Serializable {
     
-    private String id;
+    private static final long serialVersionUID = 1L;
+
+    @Id
+	private String id;
 
     private String name;
 
